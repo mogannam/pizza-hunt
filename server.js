368c43ed124092b3fbc077f23f071965f9522c98
mogannam <joemogannam@gmail.com>
2022-01-29 10:26:41 -0800

    fix heroku

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-  useFindAndModify: false,
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+//   useFindAndModify: false,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
